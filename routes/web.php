bd0d37547af85e24ce2c062f3dbc7572e6a68c4b
dharmik kamani <113373062+dharmik-kamani@users.noreply.github.com>
2026-06-15 10:30:57 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::post('/password/reset', [ProfileController::class, 'resetPassword'])->name('password.reset');
+    Route::post('/password/reset', [ProfileController::class, 'resetPassword'])->name('profile.password.reset');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/saved-blogs', [HomeController::class, 'saved'])->name('blogs.saved');
